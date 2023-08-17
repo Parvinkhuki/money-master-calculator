d9bf95979c9b00c3704ffd56c0612d8061039ed8
Parvinkhuki <khuki.developer@gmail.com>
2023-08-18 04:51:50 +0600

    done

--- index.js
+++ index.js
@@ -5,28 +5,36 @@ const food=getInputById("food")
 const rent=getInputById("rent")
 const cloth=getInputById("cloths")
 const disc=getInputById("disc")
+if(isNaN(inputs)){
+    return alert("need Income to calculate")
+   }
     const sum=food + rent + cloth
+    
 const balance= inputs-sum
+if(balance<0){
+    return alert("balance is 0 You poor)
+}
 let sums= setValue("expens",sum)
 let subs= setValue("bala",balance)
-console.log(sum)
+
 })
 document.getElementById("save").addEventListener("click",function(){
    
-const food=getInputById("food")
-const rent=getInputById("rent")
-const cloth=getInputById("cloths")
+const expens=document.getElementById("expens").innerText
+console.log(expens)
 const disc=getInputById("disc")
-
-    const sum=food + rent + cloth
-    console.log(sum)
+//    if(isNaN(disc)){
+//     return alert("need expens to calculate")
+//    }
     if(disc===20){
-       
-        console.log(sum)
+       const saving=((expens*20)/100)
+       const remain= expens- saving
+       setValue("saving",saving)
+       setValue("total",remain)
     }
     else{
         setValue("saving",0)
-        setValue("total",sum)
+        setValue("total",expens)
 
     }
     
